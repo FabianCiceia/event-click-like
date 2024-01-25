e41aa29e7966f287a55393ac6a959204c2ce82df
FabianCiceia <ciceiafabian@gmail.com>
2024-01-25 15:21:01 -0300

    agregando varios likes

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-textLike = document.querySelector("#like");
-var likes = 0;
-textLike.innerText = likes + " Likes";
+textLike1 = document.querySelector("#like-1");
+textLike2 = document.querySelector("#like-2");
+textLike3 = document.querySelector("#like-3");
 
-buttonLike = document.querySelector("#like-button");
-function clickLike(element){
-    likes+=1;
-    console.log(likes + "Likes");
-    textLike.innerText = likes + " Likes";
+
+var likes1 = 9;
+var likes2 = 12;
+var likes3 = 10;
+
+textLike1.innerText = likes1 + " Like(s)";
+textLike2.innerText = likes2 + " Like(s)";
+textLike3.innerText = likes3 + " Like(s)";
+
+
+
+
+//Funciones al apretar algunos de los botones de like 
+buttonLike1 = document.querySelector("#like-button");
+function clickLike1(element){
+    likes1+=1;
+    textLike1.innerText = likes1 + " Like(s)";
+}
+
+buttonLike2 = document.querySelector("#like-button");
+function clickLike2(element){
+    likes2+=1;
+    textLike2.innerText = likes2 + " Like(s)";
+}
+
+buttonLike3 = document.querySelector("#like-button");
+function clickLike3(element){
+    likes3+=1;
+    textLike3.innerText = likes3 + " Like(s)";
 }
